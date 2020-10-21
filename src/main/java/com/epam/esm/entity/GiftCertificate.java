654b16bab07192dcc452342eef09dc3e520595eb
Victor <vitoreshedko@gmail.com>
2020-10-21 12:19:03 +0300

    constructors, getters nd setters to GiftCertificate class

--- src/main/java/com/epam/esm/entity/GiftCertificate.java
+++ src/main/java/com/epam/esm/entity/GiftCertificate.java
@@ -1,15 +1,110 @@
 package com.epam.esm.entity;
 
 
-import javax.xml.datatype.Duration;
-import java.util.Date;
-
 public class GiftCertificate {
     private String name;
     private String description;
     private Float price;
-    private Date creationTime;
-    private Date updateTime;
+    private String creationTime;
+    private String updateTime;
     private Integer duration;
 
+    public GiftCertificate() {
+    }
+
+    public GiftCertificate(String name, String description, Float price,
+                           String creationTime, String updateTime, Integer duration) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.creationTime = creationTime;
+        this.updateTime = updateTime;
+        this.duration = duration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(String updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public String getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(String creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GiftCertificate that = (GiftCertificate) o;
+
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (description != null ? !description.equals(that.description) : that.description != null) return false;
+        if (price != null ? !price.equals(that.price) : that.price != null) return false;
+        if (creationTime != null ? !creationTime.equals(that.creationTime) : that.creationTime != null) return false;
+        if (updateTime != null ? !updateTime.equals(that.updateTime) : that.updateTime != null) return false;
+        return duration != null ? duration.equals(that.duration) : that.duration == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (price != null ? price.hashCode() : 0);
+        result = 31 * result + (creationTime != null ? creationTime.hashCode() : 0);
+        result = 31 * result + (updateTime != null ? updateTime.hashCode() : 0);
+        result = 31 * result + (duration != null ? duration.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "GiftCertificate{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", creationTime='" + creationTime + '\'' +
+                ", updateTime='" + updateTime + '\'' +
+                ", duration=" + duration +
+                '}';
+    }
 }
